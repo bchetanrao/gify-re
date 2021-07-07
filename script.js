94197c35cbb40e03b611b89e462cba474b9b37bd
bchetanrao <chetanraocr7@gmail.com>
2021-07-07 13:05:58 +0530

    updated css

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ const giphy_api_key = "DGBskRLgWTdXCUFW66yuyeEelLwevAfL";
 function getGifs() {
   /* GIPHY API Key */
   const gif = document.getElementById("gifs").value;
-  const search_endpoint = `https://api.giphy.com/v1/gifs/search?q=${gif}&api_key=${giphy_api_key}&limit=50`;
+  const search_endpoint = `https://api.giphy.com/v1/gifs/search?q=${gif}&api_key=${giphy_api_key}&limit=30`;
 
   /* Fetching Data from the API */
   fetch(search_endpoint)
     .then(api_res => api_res.json())
     .then(api_output => {
-      const gif_array = api_output.data.map(gif => gif.images.fixed_height.url);
+      const gif_array = api_output.data.map(gif => gif.images.downsized.url);
 
       /* Outputing a <img> element for every GIFs */
       const gifs = gif_array
 
         .map(
-          gif => `<div class="flex-fill"><img src=${gif}></div>`
+          gif => `<div class="d-flex flex-fill justify-content-center align-self-stretch"><img src=${gif} class="d-flex flex-fill justify-content-space-between align-self-stretch"></div>`
         )
         .join();
       /* Displaying the output */
@@ -41,6 +41,7 @@ function getGifs() {
     })
 }
 
+// perform search on pressing enter key
 document.getElementById("search_gif").addEventListener("click", getGifs);
 
 var input = document.getElementById("gifs");
@@ -51,6 +52,7 @@ input.addEventListener("keyup", function(event) {
   }
 });
 
+
 /*  GIPHY -Trending Endpoint (Fetch GIFs currently trending online) */
 
 const trending_endpoint = `https://api.giphy.com/v1/gifs/trending?api_key=${giphy_api_key}&limit=30`;
@@ -59,10 +61,10 @@ fetch(trending_endpoint)
   .then(api_res => api_res.json())
   .then(api_output => {
     const trending_gifs_array = api_output.data.map(
-      gif => gif.images.fixed_height.url
+      gif => gif.images.downsized.url
     );
     const trending_gifs = trending_gifs_array
-      .map(gif => `<img src=${gif}>`)
+      .map(gif => `<div class="d-flex flex-fill justify-content-center align-self-stretch"><img src=${gif} class="d-flex flex-fill justify-content-space-between align-self-stretch"></div>`)
       .join();
     document.getElementById("trending_gifs").innerHTML = trending_gifs;
   })
